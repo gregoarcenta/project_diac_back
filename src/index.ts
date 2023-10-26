9a310c750a3086132c8ab4581d0388a99a2efde9
Gregory Arcentales <gregoarcenta@gmail.com>
2023-10-26 01:36:05 -0500

    creacion endpoint de prueba

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
-import express from 'express';
+import express, { type Request, type Response } from "express";
+import { PrismaClient, type Prisma } from "@prisma/client";
+
+// Build 'select' object
+const userEmail: Prisma.UserSelect = {
+  email: true,
+  name: true
+};
+
+const prisma = new PrismaClient();
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", getUsers);
+
+async function getUsers(req: Request, res: Response): Promise<void> {
+  const users = await prisma.user.findMany({
+    select: userEmail
+  });
+
+  res.json(users);
+}
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
